eb3af5ae577c3725edc5100267dd34284a81a994
rubenfsgomes <ruben_fsgomes@hotmail.com>
2024-01-15 10:33:00 +0000

    Section 12

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using API.DTOs;
+using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using System.Security.Claims;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly TokenService _tokenService;
+
+        public AccountController(UserManager<AppUser> userManager, TokenService tokenService)
+        {
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized();
+
+            var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (result)
+            {
+                return CreateUserObject(user);
+            }
+
+            return Unauthorized();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDTO)
+        {
+            if (await _userManager.Users.AnyAsync(x => x.UserName == registerDTO.UserName))
+            {
+                return BadRequest("Username is already taken");
+            }
+
+            if (await _userManager.Users.AnyAsync(x => x.Email == registerDTO.Email))
+            {
+                return BadRequest("Email is already taken");
+            }
+
+            var user = new AppUser
+            {
+                DisplayName = registerDTO.Displayname,
+                Email = registerDTO.Email,
+                UserName = registerDTO.UserName,
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDTO.Password);
+
+            if (result.Succeeded)
+            {
+                return CreateUserObject(user);
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDTO>> GetCurrentUser()
+        {
+            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateUserObject(user);
+        }
+
+        private UserDTO CreateUserObject(AppUser user)
+        {
+            return new UserDTO
+            {
+                DisplayName = user.DisplayName,
+                Image = null,
+                Token = _tokenService.CreateToken(user),
+                Username = user.UserName,
+            };
+        }
+    }
+}
---
+++ API/DTOs/RegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDTO
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
+        public string Password { get; set; }
+        [Required]
+        public string Displayname { get; set; }
+        [Required]
+        public string UserName { get; set; }
+    }
+}
